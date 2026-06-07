bb0534f9722f2765b09bb10f3298ae0dbc195040
danushka <danmadushzxc@gmail.com>
2026-06-08 02:34:24 +0530

    feat:added reports module

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { SuspectViewComponent } from './pages/suspect-list/suspect-view/suspect-
 import { Home2 } from './pages/home-2/home-2';
 import { ActivityLogComponent } from './pages/activity-log/activity-log.component';
 import { ActivityLogViewComponent } from './pages/activity-log/activity-log-view/activity-log-view.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
 export const routes: Routes = [
   {
@@ -41,6 +42,11 @@ export const routes: Routes = [
   {
     path: 'activity-log/:id',
     component: ActivityLogViewComponent
+  },
+
+  {
+    path: 'reports',
+    component: ReportsComponent
   },
 
   {
