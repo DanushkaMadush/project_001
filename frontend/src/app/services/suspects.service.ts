6de0425c3128ac37fae49dd07003362315adebce
danushka <danmadushzxc@gmail.com>
2026-06-08 00:52:27 +0530

    feat:added get by id and update services

--- frontend/src/app/services/suspects.service.ts
+++ frontend/src/app/services/suspects.service.ts
@@ -5,9 +5,36 @@ import suspectsData from '../data/suspects.json';
   providedIn: 'root',
 })
 export class SuspectsService {
+  private suspects: any[] = [...(suspectsData as any[])];
 
   getSuspects(): any[] {
-    return suspectsData as any[];
+    return this.suspects;
   }
-  
+
+  getSuspectByCaseNumber(caseNumber: string): any | undefined {
+    return this.suspects.find((suspect) => suspect.CaseNumber === caseNumber);
+  }
+
+  deleteSuspectByCaseNumber(caseNumber: string): boolean {
+    const initialLength = this.suspects.length;
+    this.suspects = this.suspects.filter((suspect) => suspect.CaseNumber !== caseNumber);
+
+    return this.suspects.length !== initialLength;
+  }
+
+  updateSuspectByCaseNumber(caseNumber: string, updatedSuspect: any): boolean {
+    const suspectIndex = this.suspects.findIndex((suspect) => suspect.CaseNumber === caseNumber);
+
+    if (suspectIndex === -1) {
+      return false;
+    }
+
+    this.suspects[suspectIndex] = {
+      ...this.suspects[suspectIndex],
+      ...updatedSuspect,
+      CaseNumber: caseNumber,
+    };
+
+    return true;
+  }
 }
