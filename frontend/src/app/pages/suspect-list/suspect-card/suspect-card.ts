fa2a0778a99d0f7d65f0fd7fde31fe95ba49cf57
danushka <danmadushzxc@gmail.com>
2026-06-07 22:08:00 +0530

    feat:added suspect card

---
+++ frontend/src/app/pages/suspect-list/suspect-card/suspect-card.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-suspect-card',
+  imports: [CommonModule],
+  templateUrl: './suspect-card.html',
+  styleUrl: './suspect-card.scss',
+})
+export class SuspectCard {
+  
+  @Input() suspect: any;
+
+  get statusClass(): string {
+    const status = this.suspect?.status?.toLowerCase() || '';
+
+    if (status.includes('active') || status.includes('investigation')) return 'status-active';
+    if (status.includes('closed') || status.includes('convicted')) return 'status-closed';
+    if (status.includes('charged') || status.includes('new')) return 'status-alert';
+
+    return 'status-default';
+  }
+
+  openFile(filePath: string): void {
+    window.open(filePath, '_blank');
+  }
+}
