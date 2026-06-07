0416d7f9621f1affe6e093cf8383434d204b957b
Shehan-v <vindikas@ccl.lk>
2026-06-07 21:18:35 +0530

    Development - Add App Routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,58 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RoleManagementComponent } from './pages/role-management/role-management.component';
+import { UserManagementComponent } from './pages/user-management/user-management.component';
+
+import { SuspectListComponent } from './pages/suspect-list/suspect-list.component';
+import { SuspectAddComponent } from './pages/suspect-list/suspect-add/suspect-add.component';
+import { SuspectUpdateComponent } from './pages/suspect-list/suspect-update/suspect-update.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'role-management',
+    component: RoleManagementComponent
+  },
+
+  {
+    path: 'user-management',
+    component: UserManagementComponent
+  },
+
+  {
+    path: 'suspect-list',
+    component: SuspectListComponent
+  },
+
+  {
+    path: 'suspect-add',
+    component: SuspectAddComponent
+  },
+
+  {
+    path: 'suspect-update/:id',
+    component: SuspectUpdateComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
