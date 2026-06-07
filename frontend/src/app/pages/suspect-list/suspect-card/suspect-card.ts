fb09da8d9739cf59d82d48aacf40c6957d783b80
danushka <danmadushzxc@gmail.com>
2026-06-07 22:48:46 +0530

    refactor:changed card layout

--- frontend/src/app/pages/suspect-list/suspect-card/suspect-card.ts
+++ frontend/src/app/pages/suspect-list/suspect-card/suspect-card.ts
@@ -8,20 +8,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './suspect-card.scss',
 })
 export class SuspectCard {
-  
   @Input() suspect: any;
 
   get statusClass(): string {
-    const status = this.suspect?.status?.toLowerCase() || '';
+    const status = this.suspect?.Status?.toLowerCase() || '';
 
     if (status.includes('active') || status.includes('investigation')) return 'status-active';
     if (status.includes('closed') || status.includes('convicted')) return 'status-closed';
     if (status.includes('charged') || status.includes('new')) return 'status-alert';
 
     return 'status-default';
-  }
-
-  openFile(filePath: string): void {
-    window.open(filePath, '_blank');
   }
 }
